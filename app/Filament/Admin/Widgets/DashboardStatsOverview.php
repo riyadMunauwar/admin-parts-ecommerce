70e9a51c1aedb583a333543692967acc984e7b9f
riyad <hifazakhatun1108@gmail.com>
2023-10-26 10:10:01 +0600

    dep

--- app/Filament/Admin/Widgets/DashboardStatsOverview.php
+++ app/Filament/Admin/Widgets/DashboardStatsOverview.php
@@ -7,24 +7,31 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class DashboardStatsOverview extends BaseWidget
 {
+
+    protected static ?string $pollingInterval = null;
+
     protected function getStats(): array
     {
         return [
-            Stat::make('Revenue', '$192.1k')
-            ->description('32k increase')
-            ->descriptionIcon('heroicon-m-arrow-trending-up')
-            ->chart([7, 2, 10, 3, 15, 4, 17])
-            ->color('success'),
-            Stat::make('New customers', '1340')
-                ->description('3% decrease')
-                ->descriptionIcon('heroicon-m-arrow-trending-down')
-                ->chart([17, 16, 14, 15, 14, 13, 12])
-                ->color('danger'),
-            Stat::make('New orders', '3543')
-                ->description('7% increase')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->chart([15, 4, 10, 2, 12, 4, 12])
-                ->color('success'),
+
+            Stat::make('Total Sales', '$192.1k'),
+            // ->description('32k increase')
+            // ->descriptionIcon('heroicon-m-arrow-trending-up')
+            // ->chart([7, 2, 10, 3, 15, 4, 17])
+            // ->color('success'),
+
+            Stat::make('Total Orders', '1340'),
+
+            Stat::make('Total Products', '3543'),
+
+            Stat::make('Total Customers', '3543'),
+
+            Stat::make('Today Sales', '3543'),
+
+
+            Stat::make('Today Orders', '3543'),
+
+
         ];
     }
 }
