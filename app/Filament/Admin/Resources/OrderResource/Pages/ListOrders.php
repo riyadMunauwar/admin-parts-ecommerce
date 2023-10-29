750f8088ca8962309bc24b9eab23127d3813b3b6
riyad <hifazakhatun1108@gmail.com>
2023-10-29 10:25:39 +0600

    dep

--- app/Filament/Admin/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Admin/Resources/OrderResource/Pages/ListOrders.php
@@ -27,7 +27,21 @@ class ListOrders extends ListRecords
 
             \App\Filament\Admin\Enums\OrderStatus::New->value => Tab::make()
                 ->modifyQueryUsing(fn (Builder $query) => $query->where('status', \App\Filament\Admin\Enums\OrderStatus::New->value)),
-                
+            
+            \App\Filament\Admin\Enums\OrderStatus::Pending->value => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', \App\Filament\Admin\Enums\OrderStatus::Pending->value)),
+
+            \App\Filament\Admin\Enums\OrderStatus::Shipped->value => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', \App\Filament\Admin\Enums\OrderStatus::Shipped->value)),
+
+            \App\Filament\Admin\Enums\OrderStatus::Deliverd->value => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', \App\Filament\Admin\Enums\OrderStatus::Deliverd->value)),
+
+            \App\Filament\Admin\Enums\OrderStatus::Cancelled->value => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', \App\Filament\Admin\Enums\OrderStatus::Cancelled->value)),
+
+            \App\Filament\Admin\Enums\OrderStatus::Refunded->value => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', \App\Filament\Admin\Enums\OrderStatus::Refunded->value)),
 
         ];
     }
